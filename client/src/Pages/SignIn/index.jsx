55de1787388f2bad961d61191187e2046d209748
SheaParrott <alexandergotthis@gmail.com>
2019-01-23 17:14:34 -0500

    dektop sign in page updated

--- client/src/Pages/SignIn/index.jsx
+++ client/src/Pages/SignIn/index.jsx
@@ -10,40 +10,52 @@ class SignIn extends Component {
     return (
       <div className="columnCentering">
         <h1 className="AppName">Community</h1>
-        <div>
-          <section className="signUp columnCentering boxShadow">
-            <Link to="/login">
-              <button className="signin" onClick={this.signUp}>
-                LOG IN / SIGN UP
-              </button>
+        <main className="show">
+          <div className="flex">
+            <Link to="/login" className="white">
+              <div>LOGIN / SIGN UP</div>
             </Link>
+          </div>
+          <div className="b-lv" />
+        </main>
+        <main className="hide columnCentering">
+          <div>
+            <section className="signUp columnCentering">
+              <Link to="/login">
+                <button className="signin" onClick={this.signUp}>
+                  LOG IN / SIGN UP
+                </button>
+              </Link>
+            </section>
+          </div>
+          <section className="signInMission">
+            <div className="signInMissionStatements">
+              <i className="fas fa-hands-helping primary-text" />
+              <p className="signInMissionStatements primary-text">HELP</p>
+            </div>
+            <i className="fas fa-arrow-right primary-text" />
+            <div className="signInMissionStatements">
+              <i className="fas fa-lightbulb primary-text" />
+              <p className="signInMissionStatements primary-text">INSPIRE</p>
+            </div>
+            <i className="fas fa-arrow-right primary-text" />
+            <div className="signInMissionStatementsRight">
+              <i className="fab fa-connectdevelop primary-text" />
+              <p className="signInMissionStatementsRight primary-text">
+                CONNECT
+              </p>
+            </div>
           </section>
-        </div>
-        <section className="signInMission">
-          <div className="signInMissionStatements">
-            <i className="fas fa-hands-helping primary-text" />
-            <p className="signInMissionStatements primary-text">HELP</p>
-          </div>
-          <i className="fas fa-arrow-right primary-text" />
-          <div className="signInMissionStatements">
-            <i className="fas fa-lightbulb primary-text" />
-            <p className="signInMissionStatements primary-text">INSPIRE</p>
-          </div>
-          <i className="fas fa-arrow-right primary-text" />
-          <div className="signInMissionStatementsRight">
-            <i className="fab fa-connectdevelop primary-text" />
-            <p className="signInMissionStatementsRight primary-text">CONNECT</p>
-          </div>
-        </section>
-        <section className="mission columnCentering widthSmall">
-          <p className="missionStatement">
-            Here in the <i>Community</i> our goal is to provide a safe place to
-            help one another out and spread good vibes. This is a platform to
-            ask questions, seek out assistance, grow community, and maybe even
-            pick up a new hobby. Thanks for taking the time to read about us. We
-            hope you enjoy the <i>Community</i>.
-          </p>
-        </section>
+          <section className="mission columnCentering widthSmall">
+            <p className="missionStatement">
+              Here in the <i>Community</i> our goal is to provide a safe place
+              to help one another out and spread good vibes. This is a platform
+              to ask questions, seek out assistance, grow community, and maybe
+              even pick up a new hobby. Thanks for taking the time to read about
+              us. We hope you enjoy the <i>Community</i>.
+            </p>
+          </section>
+        </main>
       </div>
     )
   }
